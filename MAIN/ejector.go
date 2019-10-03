cf69a6b07091236cdc939feacdf8268ce8f5b5e4
YWDDDU <ywdddu@hotmail.com>
2019-10-03 23:30:05 +0800

    upgrade

--- MAIN/ejector.go
+++ MAIN/ejector.go
@@ -6,17 +6,30 @@ import (
 func main() {
 	var molec int						// 类型
 	var material int					// 材质
-	var straight_ejector_sleeve = 0		//自身司筒
-	var stepped_ejector_sleeve = 1		//有托司筒
-	var flat_ejector_pin = 2			//扁顶针
-	var normal = 0						//普通材质
-	var skd61 = 1						//国产SKD61
-	var h13 = 3							//进口SKD61
+
+	var (								//类型选择值
+		straight_ejector_sleeve = 0,	//自身司筒
+		stepped_ejector_sleeve = 1,		//有托司筒
+		flat_ejector_pin = 2,			//扁顶针
+	)
+
+	var (								//材质选择值
+		normail = 0,					//普通材质
+		skd61 = 1,						//国产SKD61
+		h13 = 3,						//进口SKD61
+	)
+
 	var ejector_size string				//司筒尺寸
+	var (
+		money_in float32				//进货价
+	)
 
 	molec = mole_choose()				//类型选择
 	material = material_choose()		//材质选择
 
 	ejector_size = size_input(molec)	//返回尺寸
+
+	money_in = ejector_math_in(ejector_size, material)
+	fmt.Printf("\t%s\t\n\t进货价: %.2f\t\n", ejector_size, money_in)
 
 }
