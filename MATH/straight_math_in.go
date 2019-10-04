40fc86f664e5f05c0285ae9d52a0a78e075c4306
YWDDDU <ywddddu@hotmail.com>
2019-10-04 12:13:14 +0800

    upgrade

--- MATH/straight_math_in.go
+++ MATH/straight_math_in.go
@@ -4,10 +4,11 @@ import (
 )
 
 func straight_math_in (size string, material int) float32 {
-	var normal_num [12][2] float32			//普通材质
-	var skd61_num [12][2] float32			//国产SKD61
+	//var normal_num [12][2] float32			//普通材质
+	//var skd61_num [12][2] float32			//国产SKD61
 	var choose_num [][] float32				//材质选择
-	normal_num = {
+
+	var normal_num = [12][2]float32{			//普通材质
 		{0.06, 0.09},
 		{0.035, 0.06},
 		{0.045, 0.07},
@@ -21,7 +22,7 @@ func straight_math_in (size string, material int) float32 {
 		{0.32, 0.48},
 		{0.40, 0.55},
 	}
-	skd61_num = {
+	var skd61_num = [12][2]float32{				//国产SKD61
 		{0.09, 0.14},
 		{0.05, 0.09},
 		{0.06, 0.10},
@@ -52,64 +53,72 @@ func straight_math_in (size string, material int) float32 {
 	// 材质 -->  单价
 	if material == 0 {
 		// 材质为国产SKD-61
-		choose_num = skd61_num
+		for i := 0; i < 12; i++ {
+			for j := 0; j < 2; j++ {
+				choose_num[i][j] = skd61_num[i][j]
+			}
+		}
 	} else if material == 2 {
 		// 材质为普通
-		choose_num = normal_num
+		for i := 0; i < 12; i++ {
+			for j := 0; j < 2; j++ {
+				choose_num[i][j] = normal_num[i][j]
+			}
+		}
 	}
 
 	i := (outside_diameter - inside_diameter) / 2		//司筒单边
 
 	if inside_diameter >= 1.0 && inside_diameter < 1.5 && outside_diameter <= 5.0 {
-		money_in = money_math(choose_num[0][0], choose_num[0][1], length0, i)
+		money_in = money_in_math(choose_num[0][0], choose_num[0][1], length0, i)
 	}
 
 	if inside_diameter >= 1.5 && inside_diameter < 4.0 && outside_diameter <= 7.0 {
-		money_in = money_math(choose_num[1][0], choose_num[1][1], length0, i)
+		money_in = money_in_math(choose_num[1][0], choose_num[1][1], length0, i)
 	}
 
 	if inside_diameter >= 4.0 && inside_diameter < 5.0 && outside_diameter <= 9.0 {
-		money_in = money_math(choose_num[2][0], choose_num[2][1], length0, i)
+		money_in = money_in_math(choose_num[2][0], choose_num[2][1], length0, i)
 	}
 
 	if inside_diameter >= 5.0 && inside_diameter < 6.0 && outside_diameter <= 9.0 {
-		money_in = money_math(choose_num[3][0], choose_num[3][1], length0, i)
+		money_in = money_in_math(choose_num[3][0], choose_num[3][1], length0, i)
 	}
 
 	if inside_diameter >= 6.0 && inside_diameter < 8.0 && outside_diameter <= 10.0 {
-		money_in = money_math(choose_num[4][0], choose_num[4][1], length0, i)
+		money_in = money_in_math(choose_num[4][0], choose_num[4][1], length0, i)
 	} else if inside_diameter >= 6.0 && inside_diameter < 8.0 && outside_diameter <= 12.0 {
-		money_in = money_math(choose_num[5][0], choose_num[5][1], length0, i)
+		money_in = money_in_math(choose_num[5][0], choose_num[5][1], length0, i)
 	}
 
 	if inside_diameter >= 8.0 && inside_diameter < 10.0 && outside_diameter <= 15.0 {
-		money_in = money_math(choose_num[6][0], choose_num[6][1], length0, i)
+		money_in = money_in_math(choose_num[6][0], choose_num[6][1], length0, i)
 	}
 
 	if inside_diameter >= 10.0 && inside_diameter < 12.0 && outside_diameter <= 18.0 {
-		money_in = money_math(choose_num[7][0], choose_num[7][1], length0, i)
+		money_in = money_in_math(choose_num[7][0], choose_num[7][1], length0, i)
 	}
 
 	if inside_diameter >= 12.0 && inside_diameter < 14.0 && outside_diameter <= 20.0 {
-		money_in = money_math(choose_num[8][0], choose_num[8][1], length0, i)
+		money_in = money_in_math(choose_num[8][0], choose_num[8][1], length0, i)
 	}
 
 	if inside_diameter >= 14.0 && inside_diameter < 16.0 && outside_diameter <= 22.0 {
-		money_in = money_math(choose_num[9][0], choose_num[9][1], length0, i)
+		money_in = money_in_math(choose_num[9][0], choose_num[9][1], length0, i)
 	}
 
 	if inside_diameter >= 16.0 && inside_diameter < 18.0 && outside_diameter <= 25.0 {
-		money_in = money_math(choose_num[10][0], choose_num[10][1], length0, i)
+		money_in = money_in_math(choose_num[10][0], choose_num[10][1], length0, i)
 	}
 
 	if inside_diameter >= 18.0 && inside_diameter < 20.0 && outside_diameter <= 28.0 {
-		money_in = money_math(choose_num[11][0], choose_num[11][1], length0, i)
+		money_in = money_in_math(choose_num[11][0], choose_num[11][1], length0, i)
 	}
 
 	return money_in
 }
 
-func money_math(ml, mm, long, i float32) float32 {
+func money_in_math(ml, mm, long, i float32) float32 {
 	var singe float32										//单价
 	if i >= 0.8 {
 		singe = long * ml
--- MATH/straight_math_out.go
+++ MATH/straight_math_out.go
@@ -3,11 +3,13 @@ import (
 	"fmt"
 )
 
-func straight_math_out (size string, material int) float32 float32 {
-	var normail_num_out [12][2] float32			//普通材质
-	var skd61_num_out [12][2] float32			//国产SKD61
-	var choose_num_out [][] float32				//材质选择
-	normal_num_out = {
+func straight_math_out (size string, material int) float32{
+	//var normail_num_out [12][2] float32			//普通材质
+	//var skd61_num_out [12][2] float32				//国产SKD61
+	var choose_num_out [][] float32					//材质选择
+	//var choose_num_out [12][2]*float32				//材质选择--指针
+
+	var normal_num_out = [12][2]float32{			//普通材质
 		{0.08, 0.11},
 		{0.05, 0.08},
 		{0.06, 0.09},
@@ -21,7 +23,7 @@ func straight_math_out (size string, material int) float32 float32 {
 		{0.35, 0.50},
 		{0.43, 0.57},
 	}
-	skd61_num_out = {
+	var skd61_num_out = [12][2]float32{				//国产SKD61
 		{0.11, 0.16},
 		{0.07, 0.11},
 		{0.08, 0.12},
@@ -38,7 +40,7 @@ func straight_math_out (size string, material int) float32 float32 {
 
 	var straight_format = "%f*%f*%f*%f"			//自身司筒格式
 	var money_out0 float32						//建议售价0
-	var money_out1 float32						//建议售价1
+	//var money_out1 float32						//建议售价1
 
 	var (
 		inside_diameter float32					//内径
@@ -54,14 +56,83 @@ func straight_math_out (size string, material int) float32 float32 {
 	// 材质 --> 单价
 	if material == 0 {
 		//材质为国产61
-		choose_num_out = skd61_num_out
+		//choose_num_out = skd61_num_out
+		for i := 0; i < 12; i++ {
+			for j := 0; j < 2; j++ {
+				choose_num_out[i][j] = skd61_num_out[i][j]
+			}
+		}
 	} else if material == 2 {
 		//材质为普通
-		choose_num_out = normail_num_out
+		//choose_num_out = normal_num_out
+		for i := 0; i < 12; i++ {
+			for j := 0; j < 2; j++ {
+				choose_num_out[i][j] = normal_num_out[i][j]
+			}
+		}
 	}
 
 	i := (outside_diameter - inside_diameter) / 2	//司筒单边
 
-	if inside_
 
+	//建议零售价0
+	if inside_diameter >= 1.0 && inside_diameter < 1.5 && outside_diameter <= 5.0 {
+		money_out0 = money_out_math(choose_num_out[0][0], choose_num_out[0][1], length0, i)
+	}
 
+	if inside_diameter >= 1.5 && inside_diameter < 4.0 && outside_diameter <= 7.0 {
+		money_out0 = money_out_math(choose_num_out[1][0], choose_num_out[1][1], length0, i)
+	}
+
+	if inside_diameter >= 4.0 && inside_diameter < 5.0 && outside_diameter <= 9.0 {
+		money_out0 = money_out_math(choose_num_out[2][0], choose_num_out[2][1], length0, i)
+	}
+
+	if inside_diameter >= 5.0 && inside_diameter < 6.0 && outside_diameter <= 9.0 {
+		money_out0 = money_out_math(choose_num_out[3][0], choose_num_out[3][1], length0, i)
+	}
+
+	if inside_diameter >= 6.0 && inside_diameter < 8.0 && outside_diameter <= 10.0 {
+		money_out0 = money_out_math(choose_num_out[4][0], choose_num_out[4][1], length0, i)
+	} else if inside_diameter >= 6.0 && inside_diameter < 8.0 && outside_diameter <= 12.0 {
+		money_out0 = money_out_math(choose_num_out[5][0], choose_num_out[5][1], length0, i)
+	}
+
+	if inside_diameter >= 8.0 && inside_diameter < 10.0 && outside_diameter <= 15.0 {
+		money_out0 = money_out_math(choose_num_out[6][0], choose_num_out[6][1], length0, i)
+	}
+
+	if inside_diameter >= 10.0 && inside_diameter < 12.0 && outside_diameter <= 18.0 {
+		money_out0 = money_out_math(choose_num_out[7][0], choose_num_out[7][1], length0, i)
+	}
+
+	if inside_diameter >= 12.0 && inside_diameter < 14.0 && outside_diameter <= 20.0 {
+		money_out0 = money_out_math(choose_num_out[8][0], choose_num_out[8][1], length0, i)
+	}
+
+	if inside_diameter >= 14.0 && inside_diameter < 16.0 && outside_diameter <= 22.0 {
+		money_out0 = money_out_math(choose_num_out[9][0], choose_num_out[9][1], length0, i)
+	}
+
+	if inside_diameter >= 16.0 && inside_diameter < 18.0 && outside_diameter <= 25.0 {
+		money_out0 = money_out_math(choose_num_out[10][0], choose_num_out[10][1], length0, i)
+	}
+
+	if inside_diameter >= 18.0 && inside_diameter < 20.0 && outside_diameter <= 28.0 {
+		money_out0 = money_out_math(choose_num_out[11][0], choose_num_out[11][1], length0, i)
+	}
+
+	//建议零售价1
+	
+	return money_out0
+}
+
+func money_out_math(ml, mm, long, i float32) float32 {
+	var singe float32										//单价
+	if i >= 0.8 {
+		singe = long * ml
+	} else if i >= 0.6 && i < 0.8 {
+		singe = long * mm
+	}
+	return singe
+}
